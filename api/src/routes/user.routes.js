4f15ed05ef57099b2ee29ba0693fe7992ee5d7e7
Gabriel Luís da Silva <officialmercurymusicdj@gmail.com>
2025-11-08 19:46:05 -0300

    fix: correct HTTP method for user route to use GET instead of POST

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -2,6 +2,6 @@ const router = require('express-promise-router')();
 const userControlller = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
-router.post('/user', authMiddleware,  userControlller.user);
+router.get('/user', authMiddleware,  userControlller.user);
 
 module.exports = router;
