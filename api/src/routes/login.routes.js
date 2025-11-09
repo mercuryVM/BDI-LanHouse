b81764e9c1e2efd680f01e7e88259e0fc604ad95
Felipe <felipefdsouza@gmail.com>
2025-11-08 22:04:55 -0300

    logout authMiddleware

--- api/src/routes/login.routes.js
+++ api/src/routes/login.routes.js
@@ -1,7 +1,8 @@
 const router = require('express-promise-router')();
 const loginControlller = require('../controllers/login.controller');
+const {authMiddleware} = require('../middlewares/auth.middleware');
 
 router.post('/login', loginControlller.login);
-router.post('/logout', loginControlller.logout);
+router.post('/logout', authMiddleware, loginControlller.logout);
 
 module.exports = router;
