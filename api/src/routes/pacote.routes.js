5bb568c3b7a01f2749c2492a0e2bb0c5eb48fa80
Felipe <felipefdsouza@gmail.com>
2025-11-15 14:57:30 -0300

    getMostBoughtPacotes

---
+++ api/src/routes/pacote.routes.js
@@ -0,0 +1,7 @@
+const router = require('express-promise-router')();
+const pacoteController = require('../controllers/pacote.controller');
+const {authMiddleware, authRequireType} = require('../middlewares/auth.middleware');
+
+router.get('/getMostBoughtPacotes', authMiddleware, authRequireType['funcionario'], pacoteController.getMostBoughtPacotes)
+
+module.exports = router;
