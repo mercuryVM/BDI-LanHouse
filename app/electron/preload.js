7c3b172f2bf8b5d3eeb564e4b385403a516b55fe
Gabriel Luís da Silva <officialmercurymusicdj@gmail.com>
2025-11-20 20:19:52 -0300

    feat: Add webFrame controls for zoom and update viewport settings in index.html

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -1,6 +1,10 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer, webFrame } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
   getMaquinaId: () => ipcRenderer.invoke('get-maquina-id'),
   launchGame: (gamePath) => ipcRenderer.invoke('launch-game', gamePath),
 });
+
+webFrame.setZoomFactor(1);
+webFrame.setVisualZoomLevelLimits(1, 1);
+webFrame.setLayoutZoomLevelLimits(0, 0);
